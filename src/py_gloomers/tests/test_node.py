044926202046bc6aecfa6e7ce12b7eccde642d1c
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-01-21 22:56:26 +0200

    test: add test for the io thing

---
+++ src/py_gloomers/tests/test_node.py
@@ -0,0 +1,36 @@
+import io
+import asyncio
+import json
+from dataclasses import asdict
+from typing import Optional
+from unittest.mock import patch
+from unittest import IsolatedAsyncioTestCase
+from py_gloomers.node import StdIOTransport, EventData
+
+
+
+ECHO_MESSAGE = "{\"src\": \"c1\", \"dest\": \"n1\", \"body\": {\"type\": \"echo\", \"msg_id\": 1, \"echo\": \"Please echo 35\"}}" # noqa
+
+
+class TestTransport(IsolatedAsyncioTestCase):
+
+    @patch("sys.stdin", io.StringIO(ECHO_MESSAGE))
+    @patch("sys.stdout", new_callable=io.StringIO)
+    async def test_read(self, stdout: io.StringIO) -> None:
+        # Given
+        loop = asyncio.get_event_loop()
+        transport = StdIOTransport(loop)
+        # When
+        data: Optional[EventData] = await transport.read()
+        # Then
+        assert data is not None
+        assert data.body["type"] == "echo"
+        # When
+        await transport.send(data)
+        # Then
+        assert stdout.getvalue().strip() == json.dumps(asdict(data))
+        # When
+        data = await transport.read()
+        # Then
+        assert data is None
+        assert transport.connection_open() is False
