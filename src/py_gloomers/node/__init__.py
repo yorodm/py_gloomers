e65c8c796f963fe142357b6c74dd313a471118b4
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-01-22 14:14:44 +0200

    feat: add some more node implementation

--- src/py_gloomers/node/__init__.py
+++ src/py_gloomers/node/__init__.py
@@ -16,15 +16,6 @@ class MessageError(Exception):
     pass
 
 
-def dataclass_from_dict(klass: Any, d: dict) -> Any:
-    """Convert dict into a dataclass."""
-    try:
-        fieldtypes = {f.name: f.type for f in fields(klass)}
-        return klass(**{f: dataclass_from_dict(fieldtypes[f], d[f]) for f in d})
-    except Exception:
-        raise MessageError()
-
-
 @dataclass
 class EventData:
     """The data for received events."""
@@ -85,7 +76,7 @@ class StdIOTransport(AbstractTransport):
                                             lambda: print(output, flush=True))
 
 
-Handler: TypeAlias = Callable[[EventData], Optional[Body]]
+Handler: TypeAlias = Callable[[Body], Optional[Body]]
 
 
 @dataclass
@@ -95,6 +86,29 @@ class Node:
     transport: AbstractTransport
     handlers: dict[str, Handler]
     loop: asyncio.AbstractEventLoop
+    node_id: Optional[str]  # Initially we have no name
+    node_ids: list[str]
+    message_count: int
+
+    def __init__(self, loop: asyncio.AbstractEventLoop,
+                 transport: AbstractTransport) -> None:
+        """Create a node and set up its internal state."""
+        self.loop = loop
+        self.transport = transport
+        self.message_count = 0
+        self.name = None  # We are using this as a marker for the init status
+
+        def init(body: Optional[Body]) -> Optional[Body]:
+            """Handle init message from the network."""
+            if self.node_id:
+                pass  # Exception we've been initialized already
+            if body is None:
+                return None  # Exception, we need a body here
+            self.node_id = body["node_id"]
+            self.node_ids = body.get("node_ids", [])
+            return {"type": "init_ok", "in_reply_to": body["msg_id"]}
+        # Register init handler
+        self.handler(init)
 
     async def start_serving(self):
         """Start the node server."""
@@ -102,16 +116,27 @@ class Node:
             data = await self.transport.read()
             self.handle(data)
 
-    def emit(self, body: Optional[Body]):
+    def emit(self, dest: str, body: Optional[Body]) -> Optional[asyncio.Handle]:
         """Emit a message back into the network."""
+        if body is None:
+            return None  # possibly an Exception
+        if self.node_id is None:
+            return None  # possibly another Exception
+        self.message_count += 1
+        body["msg_id"] = self.message_count
+        event = EventData(self.node_id, dest, body)
+        return self.loop.call_soon(self.transport.send, event)
 
     def handler(self, func: Handler):
         """Register a handler for a given message."""
+        self.handlers[func.__name__] = func
 
     def handle(self, data: dict) -> None:
         """Call the handler of the given message."""
-        event = dataclass_from_dict(EventData, data)
+        if self.node_id is None:
+            return  # Something came here before init
+        event = EventData(data["src"], data["dest"], data["body"])
         if marker := event.body.get("type", None):
-            if handler := self.handlers.get(marker, None):
-                response = handler(event.body)
-                self.emit(response)
+            if func := self.handlers.get(marker, None):
+                response = func(event.body)
+                self.emit(event.dst, response)
